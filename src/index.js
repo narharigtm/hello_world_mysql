f6166fae689a9f13a8011bac84c4e169d9e79571
root <root@ip-172-31-1-202.us-west-2.compute.internal>
2024-09-11 16:15:18 +0000

    code

---
+++ src/index.js
@@ -0,0 +1,48 @@
+require('dotenv').config(); 
+
+// Import the Express module
+const express = require('express');
+const mysql = require('mysql2');
+
+
+// Create an Express application
+const app = express();
+
+
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Try to connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Database connected successfully.');
+});
+
+
+
+// Define a route for the root URL ('/')
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const name = process.env.NAME || 'Guest'; 
+
+app.get('/name', (req, res) => {
+    res.send(`My name is ${name}`);
+});
+
+
+// Start the server on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
