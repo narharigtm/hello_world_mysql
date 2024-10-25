f01336825ea07dc96de0995dc5596e6f6fc3995c
root <root@ip-172-31-2-180.ap-northeast-2.compute.internal>
2024-10-25 02:43:24 +0000

    code

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ connection.connect((err) => {
 
 // Define a route for the root URL ('/')
 app.get('/', (req, res) => {
-    res.send('Hello, World Namaster its me DevOps Engineer Nar Hari Gautam and i Want to be MLOPS Engineer');
+    res.send('Hello, NSS');
 });
 
 const name = process.env.NAME || 'Guest'; 
